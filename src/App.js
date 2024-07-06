88b725fca8dc95aa38c2ea030c9b900a88552cd2
taeyoon0620 <woon0426@gmail.com>
2024-07-06 16:36:58 +0900

    240706 변경사항

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Container, Grid, Box, Button } from '@mui/material';
+import Header from './components/Header';
+import SideMenu from './components/SideMenu';
+import MainContent from './components/MainContent';
+import LoginArea from './components/LoginArea';
+import SNSLinks from './components/SNSLinks';
 import SignUp from './components/SignUp';
-import HomePage from './components/HomePage';
+import DarkModeToggle from './components/DarkModeToggle';
+import './App.css'; 
 
 const App = () => {
     const [darkMode, setDarkMode] = useState(false);
@@ -21,10 +28,35 @@ const App = () => {
 
     return (
         <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/signup" element={<SignUp />} />
-            </Routes>
+            <div className={`App ${darkMode ? 'dark-mode' : ''}`}>
+                <Container>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+                        </Grid>
+                        <Grid item xs={2}>
+                            <SideMenu />
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Routes>
+                                <Route path="/" element={<MainContent />} />
+                                <Route path="/signup" element={<SignUp />} />
+                            </Routes>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <LoginArea />
+                            <SNSLinks />
+                            <Box mt={2}>
+                                <Link to="/signup" style={{ textDecoration: 'none' }}>
+                                    <Button variant="contained" color="primary" fullWidth>
+                                        회원가입
+                                    </Button>
+                                </Link>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </div>
         </Router>
     );
 };
